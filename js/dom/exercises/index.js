c3eb0bb9ab6ac9dff4bfb53cccc86b98b386715e
wojciechkubiak <wgkubiak@gmail.com>
2022-02-01 21:50:39 +0100

    Control structures, algorithms, dom and more

---
+++ js/dom/exercises/index.js
@@ -0,0 +1,10 @@
+document.body.style.background = 'black';
+document.body.style.color = 'white';
+
+const setTitle = () => document.title = "Assignment - Solved!";
+
+setTitle();
+
+const header = document.getElementsByTagName('h1');
+
+header[0].innerHTML = "Assignment - Solved";
---
+++ js/testing/index.js
@@ -0,0 +1,8 @@
+
+const { printTitle } = require('./util');
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', printTitle);
+
+exports.printTitle = printTitle;
