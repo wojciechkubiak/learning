5e2cd78472860b5cccc966eb3364fa37aa80e3ab
wojciechkubiak <wgkubiak@gmail.com>
2022-02-14 00:57:55 +0100

    Redux basics completed

--- react/redux/base/src/components/Counter.js
+++ react/redux/base/src/components/Counter.js
@@ -6,26 +6,33 @@ import {
   // connect
 } from "react-redux";
 
+import { counterActions } from "../store/counter-slice";
+
 const Counter = () => {
   const dispatch = useDispatch();
 
-  const counter = useSelector((state) => state.counter);
-  const showCounter = useSelector((state) => state.showCounter);
+  const counter = useSelector((state) => state.counter.counter);
+  const showCounter = useSelector((state) => state.counter.showCounter);
 
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    // dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
   };
 
   const increaseHandler = () => {
-    dispatch({ type: "increase", amount: 5 });
+    // dispatch({ type: "increase", amount: 5 });
+    dispatch(counterActions.increase(5));
+
   };
 
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    // dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
 
   const toggleCounterHandler = () => {
-    dispatch({ type: "toggleCounter" });
+    // dispatch({ type: "toggleCounter" });
+    dispatch(counterActions.toggleCounter());
   };
 
   return (
